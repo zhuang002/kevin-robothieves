0e817d125fde3efcf6baf193425ad8eddca610e3
zhuang002 <zhuang001@gmail.com>
2020-07-28 14:56:32 -0400

    scheleton1

---
+++ src/robothieves/RoboThieves.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robothieves;
+
+/**
+ *
+ * @author zhuan
+ */
+public class RoboThieves {
+
+    /**
+     * @param args the command line arguments
+     */
+    static char[][] map=null;
+    static int[] start=new int[2];
+    static int[][] result=null;
+    public static void main(String[] args) {
+        // TODO code application logic here
+        getInput();
+        preprocessCamera();
+        process();
+        printResult();
+    }
+
+    private static void getInput() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void preprocessCamera() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void process() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void printResult() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
